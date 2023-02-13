bf1a4cf76afd5ac4fa729d93c59a2957c2ac7cf8
Raghu K <raghuk.vit@gmail.com>
2023-02-13 06:57:50 +0530

    Update roboshop-infra/

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -3,7 +3,8 @@ default_vpc_id = "vpc-075435ce9af088bfa"
 
 vpc = {
   main = {
-    cidr_block   = "10.0.0.0/16"
-    subnets_cidr = ["10.0.0.0/17", "10.0.128.0/17"]
+    cidr_block           = "10.0.0.0/16"
+    public_subnets_cidr  = ["10.0.0.0/24", "10.1.0.0/24"]
+    private_subnets_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
   }
 }
--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ module "network" {
   env            = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each     = var.vpc
-  cidr_block   = each.value.cidr_block
-  subnets_cidr = each.value.subnets_cidr
+  for_each             = var.vpc
+  cidr_block           = each.value.cidr_block
+  public_subnets_cidr  = each.value.public_subnets_cidr
+  private_subnets_cidr = each.value.private_subnets_cidr
 }
 
