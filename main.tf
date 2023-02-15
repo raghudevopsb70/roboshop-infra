a0e9bb234763b73a0f726b5d4998b159ad47dbbf
Raghu K <raghuk.vit@gmail.com>
2023-02-15 06:53:30 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -12,11 +12,12 @@ module "subnets" {
   env            = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each          = var.subnets
-  cidr_block        = each.value.cidr_block
-  availability_zone = each.value.availability_zone
-  name              = each.value.name
-  vpc_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  for_each                  = var.subnets
+  cidr_block                = each.value.cidr_block
+  availability_zone         = each.value.availability_zone
+  name                      = each.value.name
+  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
 }
 
 
