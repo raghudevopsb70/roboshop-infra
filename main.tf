a44e850270b693f95e5964436e75aaacaa8b6ee5
Raghu K <raghuk.vit@gmail.com>
2023-02-15 06:42:58 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -7,19 +7,19 @@ module "vpc" {
   cidr_block = each.value.cidr_block
 }
 
-module "subnets" {
-  source         = "github.com/raghudevopsb70/tf-module-subnets"
-  env            = var.env
-  default_vpc_id = var.default_vpc_id
-
-  for_each          = var.subnets
-  cidr_block        = each.value.cidr_block
-  availability_zone = each.value.availability_zone
-  name              = each.value.name
-  vpc_id            = lookup(module.vpc, each.value.vpc_name, null)
-}
+//module "subnets" {
+//  source         = "github.com/raghudevopsb70/tf-module-subnets"
+//  env            = var.env
+//  default_vpc_id = var.default_vpc_id
+//
+//  for_each          = var.subnets
+//  cidr_block        = each.value.cidr_block
+//  availability_zone = each.value.availability_zone
+//  name              = each.value.name
+//  vpc_id            = lookup(module.vpc, each.value.vpc_name, null)
+//}
 
 
 output "vpc_id" {
-  value = module.vpc
+  value = lookup(module.vpc, "main", null)
 }
