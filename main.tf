50509327b038d7f7d3a81e4e6414c1d97fc93f52
Raghu K <raghuk.vit@gmail.com>
2023-02-15 07:33:12 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -3,9 +3,8 @@ module "vpc" {
   env            = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each          = var.vpc
-  cidr_block        = each.value.cidr_block
-  public_subnet_ids = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  for_each   = var.vpc
+  cidr_block = each.value.cidr_block
 }
 
 module "subnets" {
@@ -19,7 +18,6 @@ module "subnets" {
   name                      = each.value.name
   vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
   vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-  internet_gw_id            = lookup(lookup(module.vpc, each.value.vpc_name, null), "internet_gw_id", null)
   internet_gw               = lookup(each.value, "internet_gw", false)
   nat_gw                    = lookup(each.value, "nat_gw", false)
 }
