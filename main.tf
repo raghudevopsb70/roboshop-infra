630036053e56747f96e85a0c072b7fc312e46ce2
Raghu K <raghuk.vit@gmail.com>
2023-02-15 06:34:04 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "network" {
+module "vpc" {
   source         = "github.com/raghudevopsb70/tf-module-vpc"
   env            = var.env
   default_vpc_id = var.default_vpc_id
@@ -6,4 +6,18 @@ module "network" {
   for_each   = var.vpc
   cidr_block = each.value.cidr_block
 }
+
+module "subnets" {
+  source         = "github.com/raghudevopsb70/tf-module-subnets"
+  env            = var.env
+  default_vpc_id = var.default_vpc_id
+
+  vpc_id = module.vpc.vpc_id
+
+  for_each          = var.subnets
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  name              = each.value.name
+}
+
 
