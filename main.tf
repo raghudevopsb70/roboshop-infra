6c7c1f51511bdd0142d87e6a6dde348c14e27097
Raghu K <raghuk.vit@gmail.com>
2023-02-17 06:53:02 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -10,23 +10,9 @@ module "vpc" {
   availability_zone = each.value.availability_zone
 }
 
-//module "subnets" {
-//  source         = "github.com/raghudevopsb70/tf-module-subnets"
+
+//module "docdb" {
+//  source         = "github.com/raghudevopsb70/tf-module-docdb"
 //  env            = var.env
-//  default_vpc_id = var.default_vpc_id
-//
-//  for_each                  = var.subnets
-//  cidr_block                = each.value.cidr_block
-//  availability_zone         = each.value.availability_zone
-//  name                      = each.value.name
-//  vpc_id                    = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-//  vpc_peering_connection_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_peering_connection_id", null)
-//  internet_gw               = lookup(each.value, "internet_gw", false)
-//  nat_gw                    = lookup(each.value, "nat_gw", false)
+//  subnet_ids     = "????"
 //}
-
-//
-output "out" {
-  value = module.vpc
-}
-
