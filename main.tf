f8ca1694db624cb00f6eb0eb07fd026faf5d37f3
Raghu K <raghuk.vit@gmail.com>
2023-02-17 07:15:06 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "docdb" {
   for_each   = var.docdb
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
   vpc_id     = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  allow_cidr = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), "app", null), "subnet_ids", null)
+  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
 }
 
 output "vpc" {
