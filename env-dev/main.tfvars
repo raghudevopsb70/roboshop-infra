891b1a7a0132d79302ad4529de1cac5bf6b7ee12
Raghu K <raghuk.vit@gmail.com>
2023-02-21 07:24:19 +0530

    Update roboshop-infra/

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -81,12 +81,14 @@ alb = {
     vpc_name     = "main"
     subnets_type = "public_subnet_ids"
     subnets_name = "public"
+    internal     = false
   }
 
   private = {
     vpc_name     = "main"
     subnets_type = "private_subnet_ids"
     subnets_name = "app"
+    internal     = true
   }
 }
 
--- main.tf
+++ main.tf
@@ -75,6 +75,7 @@ module "alb" {
   vpc_id       = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
   allow_cidr   = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
   subnets_name = each.value.subnets_name
+  internal     = each.value.internal
 }
 
 //output "vpc" {
