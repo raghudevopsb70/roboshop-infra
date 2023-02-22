2f8addca443b2d128fa120da364057dda5a7928a
Raghu K <raghuk.vit@gmail.com>
2023-02-22 06:48:18 +0530

    Update roboshop-infra/

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -101,6 +101,10 @@ apps = {
     allow_cidr_subnets_type = "public_subnets"
     allow_cidr_subnets_name = "public"
     app_port                = 80
+    max_size                = 2
+    min_size                = 1
+    desired_capacity        = 1
+    instance_type           = "t3.micro"
   }
   catalogue = {
     component               = "catalogue"
@@ -110,5 +114,9 @@ apps = {
     app_port                = 8080
     allow_cidr_subnets_type = "private_subnets"
     allow_cidr_subnets_name = "app"
+    max_size                = 2
+    min_size                = 1
+    desired_capacity        = 1
+    instance_type           = "t3.micro"
   }
 }
--- main.tf
+++ main.tf
@@ -82,12 +82,16 @@ module "apps" {
   source = "github.com/raghudevopsb70/tf-module-app"
   env    = var.env
 
-  for_each   = var.apps
-  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
-  vpc_id     = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
-  allow_cidr = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
-  component  = each.value.component
-  app_port   = each.value.app_port
+  for_each         = var.apps
+  subnet_ids       = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), each.value.subnets_type, null), each.value.subnets_name, null), "subnet_ids", null)
+  vpc_id           = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  allow_cidr       = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
+  component        = each.value.component
+  app_port         = each.value.app_port
+  max_size         = each.value.max_size
+  min_size         = each.value.min_size
+  desired_capacity = each.value.desired_capacity
+  instance_type    = each.value.instance_type
 
 }
 
