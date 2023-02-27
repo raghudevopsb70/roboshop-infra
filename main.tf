81879b3d23e6e97b03d1dac870cb129e64d43830
Raghu K <raghuk.vit@gmail.com>
2023-02-27 06:39:31 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -53,8 +53,9 @@ module "elasticache" {
 }
 
 module "rabbitmq" {
-  source = "github.com/raghudevopsb70/tf-module-rabbitmq"
-  env    = var.env
+  source       = "github.com/raghudevopsb70/tf-module-rabbitmq"
+  env          = var.env
+  bastion_cidr = var.bastion_cidr
 
   for_each           = var.rabbitmq
   subnet_ids         = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
