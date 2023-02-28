4b88b1502d11249ade7997802cc1998764480208
Raghu K <raghuk.vit@gmail.com>
2023-02-28 06:58:20 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -91,6 +91,7 @@ module "apps" {
   allow_cidr        = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), each.value.allow_cidr_subnets_type, null), each.value.allow_cidr_subnets_name, null), "cidr_block", null)
   alb               = lookup(lookup(module.alb, each.value.alb, null), "dns_name", null)
   listener          = lookup(lookup(module.alb, each.value.alb, null), "listener", null)
+  alb_arn           = lookup(lookup(module.alb, each.value.alb, null), "alb_arn", null)
   component         = each.value.component
   app_port          = each.value.app_port
   max_size          = each.value.max_size
