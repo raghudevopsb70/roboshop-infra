6970276186a151ac32329cdd66aa1b3296940397
Raghu K <raghuk.vit@gmail.com>
2023-03-17 06:10:21 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -139,29 +139,29 @@ output "vpc" {
   value = module.vpc
 }
 
-//module "minikube" {
-//  source = "github.com/scholzj/terraform-aws-minikube"
-//
-//  aws_region        = "us-east-1"
-//  cluster_name      = "minikube"
-//  aws_instance_type = "t3.medium"
-//  ssh_public_key    = "~/.ssh/id_rsa.pub"
-//  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnets", null), "public", null), "subnet_ids", null), 0)
-//  //ami_image_id        = data.aws_ami.ami.id
-//  hosted_zone         = var.hosted_zone
-//  hosted_zone_private = false
-//
-//  tags = {
-//    Application = "Minikube"
-//  }
-//
-//  addons = [
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
-//    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
-//  ]
-//}
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+
+  aws_region        = "us-east-1"
+  cluster_name      = "minikube"
+  aws_instance_type = "t3.medium"
+  ssh_public_key    = "~/.ssh/id_rsa.pub"
+  aws_subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnet_ids", null), "public", null), "subnet_ids", null), 0)
+  //ami_image_id        = data.aws_ami.ami.id
+  hosted_zone         = var.hosted_zone
+  hosted_zone_private = false
+
+  tags = {
+    Application = "Minikube"
+  }
+
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
 
 //output "MINIKUBE_SERVER" {
 //  value = "ssh centos@${module.minikube.public_ip}"
