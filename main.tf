3786aee94f95bda7f04b2a87ee0f9fcb34b5c3ce
Raghu K <raghuk.vit@gmail.com>
2023-03-17 06:12:38 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -135,9 +135,6 @@ module "alb" {
 //  }
 //}
 
-output "vpc" {
-  value = module.vpc
-}
 
 module "minikube" {
   source = "github.com/scholzj/terraform-aws-minikube"
@@ -163,10 +160,10 @@ module "minikube" {
   ]
 }
 
-//output "MINIKUBE_SERVER" {
-//  value = "ssh centos@${module.minikube.public_ip}"
-//}
-//
-//output "KUBE_CONFIG" {
-//  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
-//}
+output "MINIKUBE_SERVER" {
+  value = "ssh centos@${module.minikube.public_ip}"
+}
+
+output "KUBE_CONFIG" {
+  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
+}
