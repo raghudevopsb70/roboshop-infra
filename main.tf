a8720b8b2d7c02d08dc5307ccf4805cc0624cf80
Raghu K <raghuk.vit@gmail.com>
2023-03-23 05:50:42 +0530

    Update roboshop-infra/

--- main.tf
+++ main.tf
@@ -171,7 +171,7 @@ module "rabbitmq" {
 module "eks" {
   source             = "github.com/r-devops/tf-module-eks"
   ENV                = var.env
-  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "private_subnet_ids", null), "apps", null), "subnet_ids", null)
+  PRIVATE_SUBNET_IDS = lookup(lookup(lookup(lookup(module.vpc, "main", null), "private_subnet_ids", null), "app", null), "subnet_ids", null)
   PUBLIC_SUBNET_IDS  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnet_ids", null), "public", null), "subnet_ids", null)
   DESIRED_SIZE       = 2
   MAX_SIZE           = 2
